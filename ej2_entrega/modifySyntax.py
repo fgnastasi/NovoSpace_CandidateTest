763b0d304889bfc09bf77158d38e7bdf268f930c
Franco Gabriel Nastasi <fgnastasi@pop-os.localdomain>
2021-05-07 14:40:22 -0300

    A function is added to modifySyntax.py that export initialization memory values to a .mem file

--- ej2_entrega/modifySyntax.py
+++ ej2_entrega/modifySyntax.py
@@ -3,30 +3,26 @@
 # imports
 import re
 
-
-# TO DO: add argument functionality to script for the following variables
-inv_synt_filename = 'testcase.v'
-val_synt_filename = 'expected.v'
-mem_filename = "memdump.mem"
-
-# Regular expression for invalid syntax
-inv_synt_re = re.compile('  reg \[(.*)\] (\S*) \[(.*)\];\n  initial begin\n((    \S*\[\S*\] = \S*;\n)*)  end\n')
-#inv_synt_re = re.compile('  reg \[(?P<first_part>.*)\] (?P<second_part>\S*) \[(?P<third_part>.*)\];\n  initial begin\n(?P<men_part>(?P<memory_part>    \S*\[\S*\] = \S*;\n)*)  end\n')
-
-def create_mem_file(mem_data,mem_filename):
-    ### Inputs
-    ###     mem_data: string
-    ###     mem_count: int
-    ###     mem_filename: string
-    ###
+def create_mem_file(mem_data, mem_filename):
     ### Creates a file named mem_filename with data from  mem_data
-    pass
+    ###
+    ### Inputs
+    ###     mem_data: string - inline memory initiation as a string separated by \n
+    ###     mem_filename: string - output memory filename
+
+    mem_re = re.compile('8\'h([\da-f]+)')
+    with open(mem_filename, 'w') as mem_f:
+        for line in mem_data.splitlines():
+            mem_f.write(mem_re.search(line).group(1) + '\n')
+
 
 def main(inv_synt_filename, val_synt_filename, mem_filename, re=None):
+    ### Replaces verilog file with invalid syntax for another file with the correct(valid) syntax and creates a memory file with initialization values
+    ###
     ### Inputs
-    ###     inv_synt_filename: string - filename of verilog file with invalid syntax
-    ###     val_synt_filename: string - filename of verilog file with valid syntax
-    ###     mem_filename: string - filename of memory file where initialization data is sent
+    ###     inv_synt_filename: string - name of verilog file with invalid syntax
+    ###     val_synt_filename: string - name of verilog file with valid syntax
+    ###     mem_filename: string - name of memory file where initialization data is store
     ###     re: regular expression - regular expression according to invalid syntax
 
 
@@ -36,13 +32,24 @@ def main(inv_synt_filename, val_synt_filename, mem_filename, re=None):
         match = inv_synt_re.search(inv_synt_filedata)
         match_start, match_end = match.span()
 
+        # Writes new file with correct syntax
         with open(val_synt_filename, 'w') as val_synt_f:
             val_synt_f.write(inv_synt_filedata[0:match_start])
             val_synt_f.write("  reg [" + match.group(1) + "] " + match.group(2) + " [" + match.group(3) + "]\n")
             val_synt_f.write("  $readmemh(\""+ mem_filename + "\", " + match.group(2) + ")\n")
             val_synt_f.write(inv_synt_filedata[match_end:])
 
-    create_mem_file(match.group(4),mem_filename)
+    # Creates new file where initialization memory values are stored
+    create_mem_file(match.group(4), mem_filename)
 
 if __name__ == '__main__':
+
+    # TO DO: add argument functionality to script for the following variables
+    inv_synt_filename = 'testcase.v'
+    val_synt_filename = 'expected.v'
+    mem_filename = "memdump.mem"
+
+    # Regular expression for invalid syntax
+    inv_synt_re = re.compile('  reg \[(.*)\] (\S*) \[(.*)\];\n  initial begin\n((    \S*\[\S*\] = \S*;\n)*)  end\n')
+
     main(inv_synt_filename, val_synt_filename, mem_filename, inv_synt_re)
